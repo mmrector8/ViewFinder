a33fd18640ff5ea0dc8ca2795354177d5535df84
kaushaltheeG <kaushalsk09@gmail.com>
2023-01-16 20:54:14 -0800

    Remove comment

--- backend/validations/locations.js
+++ backend/validations/locations.js
@@ -2,7 +2,7 @@ const { check } = require("express-validator");
 const handleValidationErrors = require("./handleValidationErrors");
 
 const validateLocationInput = [
-  check("string") //check is wrong, needs to be a key/"column" name 
+  check("string")
     .exists({ checkFalsy: true })
     .isLength({ min: 2, max: 50 })
     .withMessage("Location must be between 5 and 50 characters"),
