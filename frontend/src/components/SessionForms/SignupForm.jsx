35aa46aa7c3682eb824d005af56165d9ee4ea8b1
Morgan Rector <mmrector@yahoo.com>
2023-01-16 18:14:57 -0800

    sign up modal

--- frontend/src/components/SessionForms/SignupForm.jsx
+++ frontend/src/components/SessionForms/SignupForm.jsx
@@ -2,12 +2,16 @@ import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import "./SessionForm.css";
 import { signup, clearSessionErrors } from "../../store/session";
+import "./SessionForm.css";
 
 function SignupForm() {
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [password2, setPassword2] = useState("");
+  const [bio, setBio] = useState("");
+  const [profilePicUrl, setProfilePicUrl] = useState("")
+  const [pageNum, setPageNum] = useState(1)
   const errors = useSelector((state) => state.errors.session);
   const dispatch = useDispatch();
 
@@ -33,6 +37,10 @@ function SignupForm() {
       case "password2":
         setState = setPassword2;
         break;
+      case "bio":
+        setState = setBio;
+      case "profilepicurl":
+        setState = setProfilePicUrl;
       default:
         throw Error("Unknown field in Signup Form");
     }
@@ -40,68 +48,98 @@ function SignupForm() {
     return (e) => setState(e.currentTarget.value);
   };
 
-  const usernameSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     const user = {
       email,
       username,
       password,
+      bio,
+      profilePicUrl
     };
-
-    dispatch(signup(user));
+     dispatch(signup(user))
+     setPageNum(1)
   };
 
   return (
-    <form className="session-form" onSubmit={usernameSubmit}>
-      <h2>Sign Up Form</h2>
-      <div className="errors">{errors?.email}</div>
-      <label>
-        <span>Email</span>
-        <input
-          type="text"
-          value={email}
-          onChange={update("email")}
-          placeholder="Email"
-        />
-      </label>
-      <div className="errors">{errors?.username}</div>
-      <label>
-        <span>Username</span>
-        <input
-          type="text"
-          value={username}
-          onChange={update("username")}
-          placeholder="Username"
-        />
-      </label>
-      <div className="errors">{errors?.password}</div>
-      <label>
-        <span>Password</span>
-        <input
-          type="password"
-          value={password}
-          onChange={update("password")}
-          placeholder="Password"
-        />
-      </label>
-      <div className="errors">
-        {password !== password2 && "Confirm Password field must match"}
-      </div>
-      <label>
-        <span>Confirm Password</span>
-        <input
-          type="password"
-          value={password2}
-          onChange={update("password2")}
-          placeholder="Confirm Password"
-        />
-      </label>
-      <input
-        type="submit"
-        value="Sign Up"
-        disabled={!email || !username || !password || password !== password2}
-      />
-    </form>
+    <div className="page">
+    <div className="sign-up-container">
+      <form className="session-form" onSubmit={handleSubmit}>
+        <div className="inputs-container">
+          <h2 className="form-header">Sign Up For ViewFinder</h2>
+            {pageNum === 1 ? <>
+          <span className="page-num">Page 1 of 2</span>
+          <div className="errors">{errors?.email}</div>
+          <label>
+            <input
+              type="text"
+              value={email}
+              onChange={update("email")}
+              placeholder="Email"
+            />
+          </label>
+          <div className="errors">{errors?.username}</div>
+          <label>
+            <input
+              type="text"
+              value={username}
+              onChange={update("username")}
+              placeholder="Username"
+            />
+          </label>
+          <div className="errors">{errors?.password}</div>
+          <label>
+            <input
+              type="password"
+              value={password}
+              onChange={update("password")}
+              placeholder="Password"
+            />
+          </label>
+              <label className='confirm'>
+            <input 
+              type="password"
+              value={password2}
+              onChange={update("password2")}
+              placeholder="Confirm Password"
+            />
+          </label>
+            <div className="errors">
+                {password !== password2 && "Confirm Password field must match"}
+            </div>
+              <div className='next-page-button-container'>
+                <button onClick={e=> setPageNum(2)} className='next-page-button'>Next</button>
+              </div>
+            </> : <>
+          <span className="page-num">Page 2 of 2</span><br></br>
+          <label>
+            <div className="span-padding">
+              <span>Upload a profile photo {'(optional)'}</span>
+            </div>
+            <input 
+              type="file"
+              value={profilePicUrl}
+              onChange={e=> setProfilePicUrl(e.target.value)}
+            />
+          </label>
+          <div className="errors">
+            {errors?.bio}
+          </div>
+          <label>
+                  <textarea value={bio} onChange={e => setBio(e.target.value)} placeholder="Please input a short bio (Optional)" className='bio-input' />
+          </label>
+          <div className="button-container">
+            <button onClick={(() => setPageNum(1))} className="go-back-button">Go Back</button>
+            <button className='sign-up-button'
+             type="submit"
+              disabled={!email || !username || !password || password !== password2}
+              >Sign Up</button>
+          </div>
+            </>}
+          </div>
+      </form>
+    </div>
+    </div>
   );
 }
 
