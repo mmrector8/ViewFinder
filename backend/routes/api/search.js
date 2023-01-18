ab0d6327e68cbf29af8096686269cd33f20ed753
kaushaltheeG <kaushalsk09@gmail.com>
2023-01-18 13:29:15 -0800

    Create and test search router

--- backend/routes/api/search.js
+++ backend/routes/api/search.js
@@ -13,19 +13,38 @@ router.get('/', async (req, res, next) => {
     try {
         const includedTypes = ["photos", "users", "locations"]
         if (!includedTypes.includes(req.query.type)) throw req
+        if (!req.query.body) return res.json({message: "Missing Body"})
+
         let data; 
         if (req.query.type === includedTypes[0]) {
+            //queries photo based on genre, description, condition, transportation, bestTimeOfDay, and payment
             data = await Photo.find({
-                $or: [
-                    {genre: {$regex: req.query.body}}
-                ]
-            })
+              $or: [
+                { genre: { $regex: req.query.body } },
+                { description: { $regex: req.query.body } },
+                { condition: { $regex: req.query.body } },
+                { transportation: { $regex: req.query.body } },
+                { bestTimeOfDay: { $regex: req.query.body } },
+                { payment: { $regex: req.query.body } },
+              ],
+            });
         } else if (req.query.type === includedTypes[1]) {
-
+            //queries User model based on username and email 
+            data = await User.find({
+                $or: [
+                    { username: {$regex: req.query.body } },
+                    { email: {$regex: req.query.body }}
+                ]
+            }).select("_id username email createdAt updatedAt")
+            
         } else if (req.query.type === includedTypes[2]) {
+            //queries Location model based on county 
+            data = await Location.find({
+              $or: [{ county: { $regex: req.query.body } }]
+            });
 
         }
-        return req.json(data)
+        return res.json(standardizeData(data))
     }
     catch(err) {
         const error = new Error("Search failed");
