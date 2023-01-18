4122345b6994a86208b89b68364eb295f6609941
Morgan Rector <mmrector@yahoo.com>
2023-01-17 17:38:59 -0800

    user profile page and styling

--- frontend/src/components/UserShowPage/index.js
+++ frontend/src/components/UserShowPage/index.js
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import {fetchUser} from "../../store/user"
+import "./usershow.css"
 
 const UserShowPage = () =>{
     const user = useSelector((state=> state.users))   
@@ -17,7 +18,20 @@ const UserShowPage = () =>{
     }
 
     return (
-        <h1>{user.username}</h1>
+        <div className='user-show-page-container'>
+            <div className='top-field'>
+                <img src="" alt="" className="profile-pic"></img>
+                <h1 className="profile-title">{user.username}'s Profile</h1>
+            </div>
+            <div className="user-photo-grid">
+                {/* {user.photos?.map((photo, i)=> {
+                    <img src={`${photo.url}`} alt="img grid" className="user-image-grid" key={i}></img>
+                })} */}
+                {[1,2,3,4,5,6,7,8,9].map((num, i)=> <img src="" className="user-image-grid-photo" alt='placeholder' key={i}></img>)}
+            </div>
+
+        </div>
+        
     )
 }
 
