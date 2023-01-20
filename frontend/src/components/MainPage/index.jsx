51dfb8f9d42632c8ee5e0220cdc43e2beb3c32e2
Morgan Rector <mmrector@yahoo.com>
2023-01-19 17:13:50 -0800

    Rotating background image

--- frontend/src/components/MainPage/index.jsx
+++ frontend/src/components/MainPage/index.jsx
@@ -4,12 +4,14 @@ import Footer from "../Footer";
 import MapBox from "../MapBox";
 import "./MainPage.css";
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
-import { getPhotos, fetchPhotosSplash } from "../../store/photos";
+import { useEffect, useState } from "react";
+import photosReducer, { getPhotos, fetchPhotosSplash } from "../../store/photos";
 
 const MainPage = () => {
   const locations = useSelector(getLocations);
-  const photos = useSelector(getPhotos)
+  const photos = useSelector(state=> Object.values(state.photos))
+  const [currentImgIdx, setCurrentImgIdx] = useState(0)
+  // const photos = useSelector(getPhotos)
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -21,14 +23,24 @@ const MainPage = () => {
     dispatch(fetchPhotosSplash())
   }, [dispatch])
 
+  useEffect(() => {
+    const backgroundInterval = setInterval(() => {
+      if (currentImgIdx < photos.length-1) { //3 //5, //4 //5, //5, //5
+        setCurrentImgIdx(currentImgIdx + 1)
+      } else {
+        setCurrentImgIdx(0)
+      }
+    }, 3000)
+    return () => clearInterval(backgroundInterval)
+  }, [currentImgIdx])
+
   return (
     <>
       <div className="map-and-carousel-container">
         <div className="carousel">
-          <div className="info-sidebar"></div>
-          <div className="carousel-top"></div>
-          <div className="carousel-bottom-left"></div>
-          <div className="carousel-bottom-right"></div>
+            <div className="carousel-top"><img src={photos[currentImgIdx]?.url} className="image-one"></img></div>
+           <div className="carousel-bottom-left"><img src={photos[(currentImgIdx +1)%photos.length]?.url} className="image-two"></img></div>
+          <div className="carousel-bottom-right"><img src={photos[(currentImgIdx +2)%photos.length]?.url} className="image-three"></img></div>
         </div>
         <div className="mainpage-mapbox">
           <MapBox locations={locations} />
