8b407bdae7047f49713aca272c21f0bf9cb9a8dc
kaushaltheeG <kaushalsk09@gmail.com>
2023-01-21 19:27:23 -0800

    Ready for merge

--- frontend/src/components/SessionForms/LoginModal.jsx
+++ frontend/src/components/SessionForms/LoginModal.jsx
@@ -10,7 +10,7 @@ const customStyles = {
     left: "50%",
     right: "auto",
     bottom: "auto",
-    height: "320px",
+    height: "400px",
     width: "400px",
     marginRight: "-50%",
     transform: "translate(-50%, -50%)",
