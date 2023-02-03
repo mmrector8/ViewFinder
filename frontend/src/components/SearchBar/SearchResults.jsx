f3a2f1ad3438ce4039f6cad2f88821879b01a05b
Nishant Racherla <nishant.racherla@gmail.com>
2023-02-03 15:07:48 -0800

    added icons to search results

--- frontend/src/components/SearchBar/SearchResults.jsx
+++ frontend/src/components/SearchBar/SearchResults.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
+import RoomIcon from "@mui/icons-material/Room";
+import PersonIcon from "@mui/icons-material/Person";
+import InsertPhotoIcon from "@mui/icons-material/InsertPhoto";
 import "./Search.css";
 
 const SearchResults = () => {
   const history = useHistory();
-  const dispatch = useDispatch();
   const results = useSelector((store) => {
     if (store.search) return Object.values(store.search);
     else return [];
@@ -20,36 +22,34 @@ const SearchResults = () => {
               if (result.county)
                 return (
                   <li
+                    className="result-item"
                     key={idx}
                     onClick={() => {
                       history.push(`/locations/${result._id}`);
-                      dispatch(closeSearchModal());
                     }}
                   >
-                    {result.county}
+                    <RoomIcon sx={{ mr: "5px", opacity: "0.4" }} /> {result.county}
                   </li>
                 );
               else if (result.username)
                 return (
                   <li
+                    className="result-item"
                     key={idx}
-                    onClick={() => {
-                      history.push(`/users/${result._id}`);
-                      dispatch(closeSearchModal());
-                    }}
+                    onClick={() => history.push(`/users/${result._id}`)}
                   >
+                    <PersonIcon sx={{ mr: "5px", opacity: "0.4" }} />
                     {result.username}
                   </li>
                 );
               else
                 return (
                   <li
+                    className="result-item"
                     key={idx}
-                    onClick={() => {
-                      history.push(`/spots/${result.spotId}`);
-                      dispatch(closeSearchModal());
-                    }}
+                    onClick={() => history.push(`/spots/${result.spotId}`)}
                   >
+                    <InsertPhotoIcon sx={{ mr: "5px", opacity: "0.4" }} />
                     {result.description}
                   </li>
                 );
