7258df1ed009877cbc7aef7f14376888f84dd759
celuu <christine.e.luu@gmail.com>
2023-02-02 17:03:10 -0800

    Update Search Bar

--- frontend/src/components/SearchBar/index.jsx
+++ frontend/src/components/SearchBar/index.jsx
@@ -23,23 +23,28 @@ const SearchBar = () => {
     setQueryType("");
   }, [location.pathname]);
 
+  const capitalize = (string) => string[0].toUpperCase() + string.substring(1)
+
   return (
     <form className="searchbar" onSubmit={handleSubmit}>
       <SearchIcon />
-      <input
-        type="text"
-        name="queryString"
-        placeholder="Search"
-        autoComplete="off"
-        value={queryString}
-        onChange={(e) => setQueryString(e.target.value)}
-      />
       <select value={queryType} onChange={(e) => setQueryType(e.target.value)}>
         <option value="">Search Type</option>
         <option value="users">Users</option>
         <option value="photos">Photos</option>
         <option value="locations">Locations</option>
       </select>
+        <input
+          type="text"
+          name="queryString"
+          placeholder={queryType === "" ? "Select a Search Type" : `Search ${capitalize(queryType)}`}
+          autoComplete="off"
+          disabled = {queryType === ""}
+          value={queryString}
+          onChange={(e) => setQueryString(e.target.value)}
+        />
+      
+
       <button className="navbar-button">Search</button>
     </form>
   );
